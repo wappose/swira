ac1a1ee1d93de5387548371b5cd5b919e7b80316
kjostling <kjostling@gmail.com>
2015-02-02 23:07:27 +0100

    changed font on text

--- wp-content/themes/swira/header.php
+++ wp-content/themes/swira/header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
-
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 	<link href="<?php echo get_template_directory_uri() ?>/assets/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
